1458165a84bd36b1915657b73fb44a78f144867f
zachery lorch <lorchzachery@gmail.com>
2021-08-25 12:09:55 -0700

    adding Flask REST endpoints, SQL intergration

--- src/main.py
+++ src/main.py
@@ -1,28 +1,45 @@
 # coding=utf-8
 
-from .entities.entity import Session, engine, Base
-from .entities.node import Node
+from flask import Flask, jsonify, request
 
-# generate database schema
+from .entities.entity import Session, engine, Base
+from .entities.node import Node, NodeSchema
+
+# creating the Flask application
+app = Flask(__name__)
+
+# if needed, generate database schema
 Base.metadata.create_all(engine)
 
-# start session
-session = Session()
 
-# check for existing data
-nodes = session.query(Node).all()
+@app.route('/nodes')
+def get_nodes():
+    # fetching from the database
+    session = Session()
+    node_objects = session.query(Node).all()
 
-if len(nodes) == 0:
-    # create and persist mock exam
-    python_node = Node("SQLAlchemy Node", "Test your knowledge about SQLAlchemy.", "script")
-    session.add(python_node)
-    session.commit()
+    # transforming into JSON-serializable objects
+    schema = NodeSchema(many=True)
+    nodes = schema.dump(node_objects)
+
+    # serializing as JSON
     session.close()
+    return jsonify(nodes)
 
-    # reload exams
-    nodes = session.query(Node).all()
 
-# show existing exams
-print('### nodes:')
-for node in nodes:
-    print(f'({node.id}) {node.title} - {node.description}')
+@app.route('/nodes', methods=['POST'])
+def add_node():
+    # mount node object
+    posted_node = NodeSchema(only=('title', 'description')).load(request.get_json())
+
+    node = Node(**posted_node, created_by="HTTP post request")
+
+    # persist node
+    session = Session()
+    session.add(node)
+    session.commit()
+
+    # return created node
+    new_node = NodeSchema().dump(node)
+    session.close()
+    return jsonify(new_node), 201
